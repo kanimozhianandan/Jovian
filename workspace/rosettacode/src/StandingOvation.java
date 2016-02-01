c6f354269310eca7026917de3c95f7e3a5e993f5
Kanimozhi <kanimozhimurugan@users.noreply.github.com>
2016-01-31 21:08:21 -0500

    GCJ 2015
    GCJ 2015 - Correct by small and large input

---
+++ workspace/rosettacode/src/StandingOvation.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+ * 
+ */
+
+public class StandingOvation{
+	public static void main(String[] args){
+		try{
+		Scanner sc = new Scanner(new File("/home/kani/Desktop/A-large.in"));
+		int cases = sc.nextInt();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i=0;i<cases;i++){
+		   arr.clear();	
+           int s_max = sc.nextInt();
+           String s = sc.next();
+           for(char a:s.toCharArray()) {arr.add((int) a - 48);}
+           int threshold= 0, count = 0, min_adds = 0, s_val = 0;
+           for(int j=0;j<s_max+1;j++){
+        	  s_val = arr.get(j);
+        	  if(threshold >= j) threshold += s_val;
+        	  else {count += 1; min_adds = j-threshold; threshold += min_adds + s_val;} 
+        	//  System.out.println("j:"+j+"threshols:"+threshold+"count:"+count);
+           }
+        System.out.println("Case #"+(i+1)+": "+count);   	
+		}
+	}
+	catch (Exception e){
+		
+	}
+	}
+}
